6fa7526ffdea71d8e654cc7ddb3ffd62a305aa8d
denys-bobrovnykov <62986552+denys-bobrovnykov@users.noreply.github.com>
2020-10-12 14:40:06 +0300

    Pages

---
+++ build/precache-manifest.2b6e13413a482697db0aff2c0c905b3d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c7bea49c8fc29612d848bc832a47c4",
+    "url": "/ravenous/index.html"
+  },
+  {
+    "revision": "7ae8e537e7d6665d3234",
+    "url": "/ravenous/static/css/main.0a7446e2.chunk.css"
+  },
+  {
+    "revision": "40a536335e184874c60b",
+    "url": "/ravenous/static/js/2.ff10b991.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ravenous/static/js/2.ff10b991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae8e537e7d6665d3234",
+    "url": "/ravenous/static/js/main.0f8ce532.chunk.js"
+  },
+  {
+    "revision": "9e12bad58d82799d27a7",
+    "url": "/ravenous/static/js/runtime-main.b1d203fc.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/ravenous/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/ravenous/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
